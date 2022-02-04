d5ddbff293b4006996455c384fadaf8b659fb24d
Vladimir Kotal <vlada@kotalovi.cz>
2022-02-04 23:23:01 +0100

    add basic functionality

--- README.md
+++ README.md
@@ -2,13 +2,27 @@
 
 convert MQTT events to Grafana annotations
 
-- create the `headers.json` file. It may look like this:
-```json
-{
-  "Content-type": "application/json",
-  "": ""
-}
+## Grafana
+
+- Generate API key
+- Install Mosquito MQTT broker:
 ```
+sudo apt install -y mosquitto mosquitto-clients
+cat << EOF | sudo tee /etc/mosquitto/conf.d/local.conf
+allow_anonymous true
+listener 1883
+EOF
+sudo systemctl restart mosquitto
+systemctl status mosquitto
+```
+- Test basic MQTT functionality:
+```
+mosquitto_sub -d -t test
+mosquitto_pub -d -t test -m "Hello, World!"
+```
+
+## Install and setup
+
 - pre-requisites:
 ```
 sudo apt-get install -y python3-venv
@@ -23,9 +37,24 @@ python3 -m venv env
 . ./env/bin/activate
 pip install -r requirements.txt
 ```
+- create the `/srv/mq2anno/headers.json` file. This JSON file will contain the headers sent with the Grafna API requests. Use the Grafana API key to setup the `Authorization` header. It may look like this:
+```json
+{
+  "Content-type": "application/json",
+  "Authorization": "Bearer <ENTER_THE_API_KEY_HERE>"
+}
+```
+- create the `/srv/mq2anno/payload.json` file. This JSON file can look like this:
+```json
+{
+  "dashboardId":4,
+  "tags":["tag1","tag2"],
+  "text":"Annotation Description"
+}
+```
 - configure the service: create `/srv/mq2anno/environment` file and setup these environment variables inside:
   - `ARGS`: arguments to the `mq2anno` program
-    - the `-H`, `-U` and XXX are required
+    - the `-H`, `-U` and MQTT broker hostname/port arguments are required
 - setup the service (assumes the `pi` user)
 ```
   sudo cp /srv/mq2anno/mq2anno.service /etc/systemd/system/
--- mq2anno.py
+++ mq2anno.py
@@ -1,19 +1,113 @@
 #!/usr/bin/env python3
 """
-subscripe to MQTT events and turn them into Grafana annotations
+subscribe to MQTT events and turn them into Grafana annotations
 """
 
 import argparse
 import json
 import logging
 import sys
+import time
 
+import paho.mqtt.client as mqtt
 import requests
 
 from logutil import LogLevelAction
 
 
+class FatalError(Exception):
+    """
+    exception that should prompt clean exit
+    """
+
+
+# pylint: disable=unused-argument
+def on_connect(client, userdata, flags, ret_code):
+    """
+    called on MQTT connect
+    :param client:
+    :param userdata:
+    :param flags:
+    :param rc:
+    :return:
+    """
+    logger = logging.getLogger(__name__)
+
+    if ret_code == 0:
+        logger.info("Connected to MQTT broker")
+    else:
+        logger.error(f"Connected to MQTT broker failed with code {ret_code}")
+        raise FatalError("cannot connect to MQTT broker")
+
+    client.subscribe(userdata.topic)
+
+
+# pylint: disable=unused-argument
+def on_message(client, userdata, msg):
+    """
+    called on MQTT message received
+    :param client:
+    :param userdata:
+    :param msg:
+    :return:
+    """
+    logger = logging.getLogger(__name__)
+
+    logger.debug(f"received: {msg.topic} {msg.payload}")
+
+    create_annotation(userdata, msg.payload.decode("utf-8)"))
+
+
+def create_annotation(userdata, tag):
+    """
+    :param userdata: Userdata instance
+    :param tag: string tag
+    :return:
+    """
+
+    logger = logging.getLogger(__name__)
+
+    payload = userdata.payload
+    time_start = int(time.time() * 1000)
+    payload["time"] = int(time_start)
+    payload["timeEnd"] = int(time_start) + 500
+    if not payload.get("tags"):
+        payload["tags"] = []
+    payload["tags"].append(tag)
+    url = userdata.grafana_url + "/api/annotations"
+    payload_str = json.dumps(payload)
+    logger.debug(f"posting {payload_str} with {userdata.headers} to {url}")
+    try:
+        response = requests.post(url, data=payload_str, headers=userdata.headers)
+        if response.status_code != 200:
+            logger.error(f"Failed to post {payload_str} to {url}: {response.text}")
+    except requests.exceptions.ConnectionError as exc:
+        logger.error(f"failed to post: {exc}")
+
+
+# pylint: disable=too-few-public-methods
+class Userdata:
+    """
+    configuration for MQTT handling
+    """
+
+    def __init__(self, topic, grafana_url, payload, headers):
+        """
+        :param topic: MQTT topic
+        :param grafana_url: Grafana base URL
+        :param payload: dictionary template for the payload to send to Grafana
+        :param headers: HTTP headers
+        """
+        self.topic = topic
+        self.grafana_url = grafana_url
+        self.payload = payload
+        self.headers = headers
+
+
 def main():
+    """
+    main CLI
+    """
     parser = argparse.ArgumentParser(
         description="convert MQTT events to Grafana annotations",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -21,7 +115,6 @@ def main():
     parser.add_argument(
         "-H",
         "--headers",
-        required=True,
         default="headers.json",
         help="JSON file with headers",
     )
@@ -39,11 +132,24 @@ def main():
         help="base URL for the Grafana server",
     )
     parser.add_argument(
+        "-t",
+        "--topic",
+        required=True,
+        help="MQTT topic to subscribe to",
+    )
+    parser.add_argument(
+        "-p",
+        "--payload",
+        default="payload.json",
+        help="JSON file with Grafana payload template",
+    )
+    parser.add_argument(
         "mqtt_hostname",
         help="hostname of the MQTT broker",
     )
     parser.add_argument(
         "mqtt_port",
+        type=int,
         help="port of the MQTT broker",
     )
     args = parser.parse_args()
@@ -53,9 +159,32 @@ def main():
     logger.setLevel(args.loglevel)
     logger.info("Running")
 
+    with open(args.headers, encoding="utf-8") as file_handle:
+        headers = json.load(file_handle)
+        logger.debug(f"Got headers: {headers}")
+
+    with open(args.payload, encoding="utf-8") as file_handle:
+        payload = json.load(file_handle)
+        logger.debug(f"Got payload: {payload}")
+
+    client = mqtt.Client()
+    user_data = Userdata(args.topic, args.grafana_url, payload, headers)
+    client.user_data_set(user_data)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    try:
+        keep_alive = 60
+        client.connect(args.mqtt_hostname, args.mqtt_port, keep_alive)
+    except OSError as exc:
+        raise FatalError from exc
+    client.loop_forever()
+
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
         sys.exit(0)
+    except FatalError as fatal_exc:
+        logging.getLogger(__name__).fatal(f"cannot continue: {fatal_exc}")
+        sys.exit(1)
