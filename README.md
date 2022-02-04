424f4a0b50fdc63a964eb1eda64389a49687b8a2
Vladimir Kotal <vlada@kotalovi.cz>
2022-02-04 22:04:02 +0100

    add boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # mq2anno
+
 convert MQTT events to Grafana annotations
+
+- create the `headers.json` file. It may look like this:
+```json
+{
+  "Content-type": "application/json",
+  "": ""
+}
+```
+- pre-requisites:
+```
+sudo apt-get install -y python3-venv
+```
+- install:
+```
+sudo mkdir /srv
+sudo apt-get install -y git
+git clone https://github.com/vladak/mq2anno /srv/mq2anno
+cd /srv/mq2anno
+python3 -m venv env
+. ./env/bin/activate
+pip install -r requirements.txt
+```
+- configure the service: create `/srv/mq2anno/environment` file and setup these environment variables inside:
+  - `ARGS`: arguments to the `mq2anno` program
+    - the `-H`, `-U` and XXX are required
+- setup the service (assumes the `pi` user)
+```
+  sudo cp /srv/mq2anno/mq2anno.service /etc/systemd/system/
+  sudo systemctl enable mq2anno
+  sudo systemctl daemon-reload  # in case the service file changed
+  sudo systemctl start mq2anno
+  sudo systemctl status mq2anno
+```
+
+
---
+++ mq2anno.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+subscripe to MQTT events and turn them into Grafana annotations
+"""
+
+import logging
+import argparse
+import json
+import requests
+import sys
+
+from logutil import LogLevelAction
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="convert MQTT events to Grafana annotations",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "-H",
+        "--headers",
+	required=True,
+	default="headers.json",
+        help="JSON file with headers",
+    )
+    parser.add_argument(
+        "-l",
+        "--loglevel",
+        action=LogLevelAction,
+        help='Set log level (e.g. "ERROR")',
+        default=logging.INFO,
+    )
+    parser.add_argument(
+        "-U",
+	dest='grafana_url',
+	required=True,
+        help="base URL for the Grafana server",
+    )
+    parser.add_argument(
+	'mqtt_hostname',
+        help="hostname of the MQTT broker",
+    )
+    parser.add_argument(
+	'mqtt_port',
+        help="port of the MQTT broker",
+    )
+    args = parser.parse_args()
+
+    logging.basicConfig()
+    logger = logging.getLogger(__package__)
+    logger.setLevel(args.loglevel)
+    logger.info("Running")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(0)
