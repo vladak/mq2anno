986715ed69be5074704b9cc923fa23267758bc30
Vladimir Kotal <vlada@kotalovi.cz>
2024-02-14 11:49:23 +0100

    add negative tests

--- test_payload.py
+++ test_payload.py
@@ -13,7 +13,7 @@ class TestPayloadHandling(unittest.TestCase):
     """payload handling tests"""
 
     @patch("mq2anno.requests")
-    def test_payload_processing(self, mock_requests):
+    def test_payload_positive(self, mock_requests):
         """
         Verify that payload handling in on_message() works correctly for the positive case.
         """
@@ -39,3 +39,28 @@ class TestPayloadHandling(unittest.TestCase):
         mock_requests.post.assert_called_once()
         assert len(mock_requests.post.call_args) >= 1
         assert mock_requests.post.call_args[0][0] == url + "/api/annotations"
+
+    @patch("mq2anno.requests")
+    def test_payload_negative(self, mock_requests):
+        """
+        Verify that payload handling in on_message() works correctly for the negative cases.
+        """
+        payloads = [
+            {"annotation": True},
+            {"annotation": False},
+            {"annotation": False, "tags": ["foo"]},
+            {"annotation": True, "tags": "bar"},
+        ]
+        for payload_dict in payloads:
+            payload = json.dumps(payload_dict)
+            with self.subTest(payload):
+                msg = Mock()
+                msg.payload = payload
+                msg.topic = "foo"
+                topic_cfg = {msg.topic: {"aaa": "bbb"}}
+                url = "http://localhost:8080"
+                userdata = Userdata(topic_cfg, url, {})
+
+                on_message(None, userdata, msg)
+
+                mock_requests.post.assert_not_called()
